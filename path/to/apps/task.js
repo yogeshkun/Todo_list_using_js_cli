94c358be76f9e4de9e9d7b3c8751372965531b9a
Yogesh <yogeshkunkawlekar12@gmail.com>
2023-02-14 15:50:18 +0530

    Second commit

--- path/to/apps/task.js
+++ path/to/apps/task.js
@@ -1,84 +1,137 @@
-// Handling text file
 const fs = require("fs");
 
-//Read file pending list
-var pending_file_path = "./path/to/plans/task.txt";
+var task_file_path = "./path/to/plans/task.txt";
 var complete_file_path = "./path/to/plans/complete.txt";
 
-//Creating empty variable to store the file
 let fileContents;
 let completeContents;
 
-let InitialValue = () => {
-  fileContents = fs.readFileSync(pending_file_path, "utf-8");
+//read task.txt file
+// let readtask = () => {
 
-  if (fileContents.length != 0) {
-    let lines = fileContents.slice(0, fileContents.length - 1).split("\n");
-    let objects = [];
+//   fileContents = fs.readFileSync(task_file_path, "utf-8")
 
-    lines.forEach((line) => {
-      let [number, string] = line.split(/\s(.+)/);
+//   if (fileContents.length != 0) {
+//     // let lines = fileContents.slice(0, fileContents.length - 1).split("\n");
+//     let lines = fileContents.split("\n");
+//     lines.pop()
+//     console.log(fileContents);
+//     let objects = [];
+
+//     lines.forEach((line) => {
+//       let [number, string] = line.split(/\s(.+)/); // Match a single whitespacecharacter
+//       objects.push({
+//         priority: Number(number),
+//         task: string,
+//       });
+//     });
+//     objects.sort((a, b) => a.priority - b.priority);
+//     fileContents = objects;
+//   }
+// };
+
+function readtask() {
+  var objects = [];
+  var task = fs.readFileSync(task_file_path, "utf-8").split("\n")
+
+  if(task.length < 1){
+      
+      task = task
+
+  }else{
+      
+      task = task.slice(0, -1); 
+
+  }
+
+  
+  task.forEach((line) => {
+      let [number, string] = line.split(/\s(.+)/); // Match a single whitespacecharacter
       objects.push({
         priority: Number(number),
         task: string,
       });
     });
-    objects.sort((a, b) => a.priority - b.priority);
-    fileContents = objects;
+  
+  objects.sort((a, b) => a.priority - b.priority);
+  return objects;
+}
+// };
+// readtask();
+
+//read complete.txt file
+function readComplete() {
+  var task = fs.readFileSync(complete_file_path, "utf-8").split("\n")
+  
+  
+  if(task.length < 1){
+    console.log(task)
+    return task
+  }else{
+    return task.slice(0, -1);
   }
-};
-InitialValue();
+  //read -> split -> delete_last element
+  // completeContents = dataArray.slice(0, dataArray.length - 1);
+  // console.log(dataArray);
+}
+// readComplete();
 
-//Read file completed list
-
-let CompletedValue = () => {
-  let dataArray;
-  dataArray = fs.readFileSync(complete_file_path, "utf-8");
-  completeContents = dataArray.slice(0, dataArray.length - 1).split("\n");
-};
-CompletedValue();
-
-//Taking arguments
-const args = process.argv;
-Check_Function(args);
-
-function Check_Function(args) {
-  switch (args[2]) {
-    case "add":
-      AddTask(args);
-      InitialValue();
-      break;
-    case "ls":
-      InitialValue();
-      Displayls(fileContents);
-      break;
-    case "done":
-      DoneTask(args, fileContents);
-      break;
-    case "help":
-      console.log(TaskUsage());
-      break;
-    case undefined:
-      console.log(TaskUsage());
-      break;
-    case "del":
-      del(args, fileContents);
-      break;
-    case "report":
-      report(fileContents, completeContents);
-      break;
-    case "DeleteAllTest":
-      DeleteAllTest();
-      break;
-    case "DeleteAllCompletedTest":
-      DeleteAllCompletedTest();
-  }
+// done -> complete &   add -> task
+function appendTask(filename, task) {
+  var appendtask = fs.createWriteStream(filename, { flags: "a" });
+  appendtask.write(task+"\n"); //
+  appendtask.end();
 }
 
-function Displayls(datas) {
-  if (fileContents.length > 0) {
-    let str = "";
-    datas.map((val, index) => {
+function writeTasks(filename, object) {
+  var logger = fs.createWriteStream(filename, { flags: "w" });
+  object.forEach(function (row) {
+    logger.write(row.priority + " " + row.task + "\n");
+    // logger.write(`${content}`);
+  });
+  logger.end();
+}
+
+//Taking arguments from cli
+
+const args = process.argv;
+
+switch (args[2]) {
+  case "add":
+    add(args);
+    // readtask();
+    break;
+  case "ls":
+    // readtask();
+    ls();
+    break;
+  case "done":
+    done(args[3]);
+    break;
+  case "help":
+    console.log(help());
+    break;
+  case undefined:
+    console.log(help());
+    break;
+  case "del":
+    del(args[3]);
+    break;
+  case "report":
+    report(fileContents, completeContents);
+    break;
+  case "DeleteAllTest":
+    DeleteAllTest();
+    break;
+  case "DeleteAllCompletedTest":
+    DeleteAllCompletedTest();
+}
+
+// To display the pending list
+function ls() {
+  var pendingTasks = readtask();
+  if (pendingTasks.length > 0) {
+    pendingTasks.map((val, index) => {
       console.log(
         (index + 1).toString() +
           ". " +
@@ -87,95 +140,48 @@ function Displayls(datas) {
           val.priority.toString() +
           "]"
       );
-      str +=
-        (index + 1).toString() +
-        ". " +
-        val.task +
-        " [" +
-        val.priority.toString() +
-        "]\n";
     });
-    let exp = `1. the thing i need to do [1]
-    2. water the plants [2]
-    3. find needle in the haystack [3]
-    `;
-    
   } else {
     console.log("There are no pending tasks!");
   }
 }
 
-// Add task
-function AddTask(args) {
+//To add new task and its priority in pending list
+function add(args) {
   if (args[3] == undefined || args[4] == undefined) {
     console.log("Error: Missing tasks string. Nothing added!");
   } else {
     let content = args[3] + " " + args[4] + "\n";
-    var logger = fs.createWriteStream(pending_file_path, {
-      flags: "a",
-    });
-    if (fileContents.length > 0) {
-      const writeLine = (line) => logger.write(`${line}`);
-      writeLine(content);
-    } else {
-      logger.write(content);
-    }
 
-    logger.end(); // close string
-
+    appendTask(task_file_path, content);
     var ReturnStatment =
       'Added task: "' + args[4] + '" with priority ' + args[3];
     console.log(ReturnStatment);
   }
 }
 
-function DoneTask(args, pendingList) {
-  const TaskNo = parseInt(args[3]);
-  console.log("task no ", typeof TaskNo);
-  if (isNaN(TaskNo) || TaskNo === "") {
+//Removing task from pending list and adding it in completed list
+function done(taskNo) {
+  var pendingTasks = readtask();
+  // var taskNo = parseInt(args[3]);
+  if (isNaN(taskNo) || taskNo === "") {
     console.log("Error: Missing NUMBER for marking tasks as done.");
   } else {
-    if (TaskNo > 0 && TaskNo < pendingList.length + 1) {
-      let task = pendingList[TaskNo - 1];
-      pendingList.splice(TaskNo - 1, 1);
-      pending_list(pendingList);
-      completed_list(task.task);
+    if (taskNo > 0 && taskNo < pendingTasks.length + 1) {
+      let task = pendingTasks.splice(taskNo - 1, 1);
+      writeTasks(task_file_path, pendingTasks); // to task.txt
+      appendTask(complete_file_path, task[0].task); // a
       console.log("Marked item as done.");
     } else {
       console.log(
-        "Error: no incomplete item with index #" + TaskNo + " exists."
+        "Error: no incomplete item with index #" + taskNo + " exists."
       );
-      Displayls(fileContents);
-    }
-
-    function pending_list(List) {
-      var logger = fs.createWriteStream(pending_file_path, {
-        flags: "w",
-      });
-      List.forEach(function (object) {
-        let content = object.priority + " " + object.task + "\n";
-
-        // console.log("Pending list\n", content);
-
-        logger.write(`${content}`);
-      });
-      logger.end(); // close string
-    }
-
-    function completed_list(completed_task) {
-      var logger = fs.createWriteStream(complete_file_path, {
-        flags: "a",
-      });
-
-      logger.write(`${completed_task + "\n"}`);
-
-      logger.end(); // close string
     }
   }
-  // console.log(fileContents)
 }
 
-function TaskUsage() {
+//
+function help() {
   let usage = `Usage :-
 $ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
 $ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
@@ -187,78 +193,77 @@ $ ./task report               # Statistics`;
   return usage;
 }
 
-//Delete particular Task
-
-function del(args, pendingList) {
-  const TaskNo = parseInt(args[3]);
-
-  if (isNaN(TaskNo) || TaskNo === "") {
+function del(taskNo) {
+  if (isNaN(taskNo) || taskNo === "") {
     console.log("Error: Missing NUMBER for deleting tasks.");
   } else {
-    if (TaskNo > 0 && TaskNo < pendingList.length + 1) {
-      pendingList.splice(TaskNo - 1, 1);
-      pending_list(pendingList);
-      function pending_list(List) {
-        var logger = fs.createWriteStream(pending_file_path, {
-          flags: "w",
-        });
-        List.forEach(function (object) {
-          let content = object.priority + " " + object.task + "\n";
+    var pendingTasks = readtask();
+    if (taskNo > 0 && taskNo < pendingTasks.length + 1) {
+      pendingTasks.splice(taskNo - 1, 1);
+      writeTasks(task_file_path, pendingTasks); // to task.txt
 
-          // console.log("Pending list\n", content);
-
-          logger.write(`${content}`);
-        });
-        logger.end(); // close string
-      }
-      console.log("Deleted task #" + TaskNo);
+      console.log(`Deleted task #${taskNo}`);
     } else {
       console.log(
         "Error: task with index #" +
-          TaskNo +
+          taskNo +
           " does not exist. Nothing deleted."
       );
-      Displayls(fileContents);
     }
   }
 }
-//Display Pending and Completed Task
-function report(pending_list, completed_list) {
-  console.log("Pending : " + pending_list.length);
-  // console.log("\n")
-  if (pending_list.length > 0) {
-    pending_list.map((val, index) => {
+
+//Creates report of pending and completed task
+function report() {
+  var pendingTasks = readtask();
+  var completedTasks = readComplete();
+
+  console.log("\nPending : " + pendingTasks.length);
+  if (pendingTasks.length > 0) {
+    pendingTasks.map((val, index) => {
       console.log(index + 1 + ". " + val.task + " [" + val.priority + "]");
     });
   } else {
     console.log("There are no pending tasks!");
   }
-  console.log("\nCompleted : " + completed_list.length);
-  // console.log("\n")
-  if (completed_list.length > 0) {
-    completed_list.map((val, index) => {
+
+  console.log("\nCompleted : " + completedTasks.length);
+  if (completedTasks.length > 0) {
+    completedTasks.map((val, index) => {
       console.log(index + 1 + ". " + val);
     });
   } else {
-    console.log("There are no pending tasks!");
+    console.log("There are no compeleted tasks!");
   }
 }
 
-//Delete all
+//Delete all task from task.txt
 function DeleteAllTest() {
-  var logger = fs.createWriteStream(pending_file_path, {
+  var logger = fs.createWriteStream(task_file_path, {
     flags: "w",
   });
-  logger.end(); // close string
-
-  console.log("*****************************\n");
+  logger.end();
 }
 
+//Delete all task from complete.txt
 function DeleteAllCompletedTest() {
   var logger = fs.createWriteStream(complete_file_path, {
     flags: "w",
   });
-  logger.end(); // close string
-
-  console.log("*****************************\n");
+  logger.end();
 }
+
+// Appending the task in file
+
+
+// del -> task & done -> task working same
+// function pending_list(List) {
+//   var logger = fs.createWriteStream(task_file_path, {
+//     flags: "w",
+//   });
+//   List.forEach(function (object) {
+//     let content = object.priority + " " + object.task + "\n";
+//     logger.write(`${content}`);
+//   });
+
+// }
